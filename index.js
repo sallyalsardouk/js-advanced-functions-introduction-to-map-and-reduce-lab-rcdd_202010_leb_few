a0b4ab4b6fe95cf38449fc38f18c5c38e1599791
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-07 10:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,27 @@ function mapToNegativize(arr){
   }
   return arr1
 }
+ 
+ function mapToNoChange(arr){
+   let arr2=[]
+   for(let i=0;i<arr.length;i++){
+     arr2[i]=arr[i]
+   }
+   return arr2
+ }
+ 
+ function mapToDouble(arr){
+   let arr3=[]
+   for(i=0;i<arr.length;i++){
+     arr3[i]=arr[i]*2
+   }
+   return arr3
+ }
+ 
+ function mapToSquare(arr){
+   let arr4=[]
+   for(let i=0;i<arr.length;i++){
+     arr4[i]=arr[i]*arr[i]
+   }
+   return arr4
+ }
