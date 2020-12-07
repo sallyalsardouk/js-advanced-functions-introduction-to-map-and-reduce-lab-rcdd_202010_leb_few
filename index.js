0be6c80af36f7afa63ea89f4bd768ade1804aedb
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-07 10:14:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function mapToNegativize(arr){
  
  function reduceToAllTrue(arr){
    for(let i=0;i<arr.length;i++){
-     if(!=arr[i]){
+     if(!arr[i]){
        return false
      }
      return true
